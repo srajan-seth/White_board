7d8a9e8a9667c1e600c9eafcc4977380f350fcbc
Srajan Seth <srajanseth39@gmail.com>
2025-11-17 20:55:02 +0530

    second

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ connectToDB();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000"], 
+      origin: ["http://localhost:3000","https://whiteboardproject.vercel.app/"], 
       methods: ["GET", "POST"],
     },
   });
